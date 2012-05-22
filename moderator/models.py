1f1d548a817d5c2dd698a3710573eade41f1a3dc
Shaun Sephton <shaun@28lines.com>
2012-05-22 17:34:29 +0200

    added classifier state, silenced save for now

--- moderator/models.py
+++ moderator/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class ClassifiedComment(models.Model):
     comment = models.ForeignKey('comments.Comment')
     cls = models.CharField(
@@ -7,16 +8,23 @@ class ClassifiedComment(models.Model):
         choices=(
             ('spam', 'Spam'),
             ('ham', 'Ham'),
-            ('noteworthy', 'Noteworthy'),
-            ('unknown', 'Unknown'),
+            ('unsure', 'Unsure'),
         )
     )
 
-    def save(self, *args, **kwargs):
-        XXX not when created to prevent poison
-        super(ClassifiedComment, self).save(*args, **kwargs)
-        from moderator import classifier
-        classifier.train(self.comment, self.cls)
+    #def save(self, *args, **kwargs):
+    #    #XXX not when created to prevent poison
+    #    super(ClassifiedComment, self).save(*args, **kwargs)
+    #    from moderator import classifier
+    #    classifier.train(self.comment, self.cls)
+
+
+class ClassifierState(models.Model):
+    """
+    Stores state (number of ham and spam trained) for classifier type.
+    """
+    spam_count = models.IntegerField()
+    ham_count = models.IntegerField()
 
 
 class Word(models.Model):
