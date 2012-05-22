b0e6f32ce95e3cc7ead68b1c54640715b9c74913
Shaun Sephton <shaun@28lines.com>
2012-05-22 17:34:02 +0200

    corrections

--- test_settings.py
+++ test_settings.py
@@ -1,6 +1,15 @@
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = 'motswadi_test.sqlite'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.sqlite', 
+    }
+}
 
 INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.comments',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+
     'moderator',
 )
