4ee711c8eb4e0269def4bcf56133cfd0294c72b0
Shaun Sephton <shaun@28lines.com>
2012-05-23 13:10:30 +0200

    correctly implemented save

--- moderator/models.py
+++ moderator/models.py
@@ -12,11 +12,29 @@ class ClassifiedComment(models.Model):
         )
     )
 
-    #def save(self, *args, **kwargs):
-    #    #XXX not when created to prevent poison
-    #    super(ClassifiedComment, self).save(*args, **kwargs)
-    #    from moderator import classifier
-    #    classifier.train(self.comment, self.cls)
+    def save(self, *args, **kwargs):
+        created = not self.pk
+        if not created:
+            previous_cls = ClassifiedComment.objects.get(pk=self.pk).cls
+        super(ClassifiedComment, self).save(*args, **kwargs)
+
+        # Don't train on initial creation to prevent circular training.
+        # Initial creation is done by classifycomments management command.
+        # We only want to train when a human has made a classification.
+        # Don't train when cls is unsure as we can't classify anyway.
+        if created or self.cls == 'unsure':
+            return
+        else:
+            # Only train when a cls changed.
+            if self.cls != previous_cls:
+                from moderator import utils
+                if self.cls == 'spam':
+                    utils.train(self.comment, is_spam=True)
+                    return
+                if self.cls == 'ham':
+                    utils.train(self.comment, is_spam=False)
+                    return
+                raise Exception("Unhandled classifications.")
 
 
 class ClassifierState(models.Model):
