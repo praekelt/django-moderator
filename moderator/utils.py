4873ea10179a1fa5666295a01be87bd11d4eae7f
Shaun Sephton <shaun@28lines.com>
2012-07-24 16:52:22 +0200

    added classify_comment method, the main entry to classify comments

--- moderator/utils.py
+++ moderator/utils.py
@@ -9,6 +9,9 @@ def train(comment, is_spam):
 
 
 def get_class(comment):
+    """
+    Returns comment class as determined by Baysian classifier.
+    """
     try:
         score = classifier.score(comment.comment)
     except ZeroDivisionError:
@@ -19,3 +22,65 @@ def get_class(comment):
     if score > getattr(settings, 'MODERATOR', DEFAULT_CONFIG)['SPAM_CUTOFF']:
         return 'spam'
     return 'unsure'
+
+
+def classify_comment(comment, cls=None):
+    """
+    Trains Baysian inference classifier with comment and provided class
+    (either ham or spam).
+
+    If no class is provided a class is determined based on user abuse reports.
+    If determined to be spam the Baysian inference classifier is trained with
+    the comment as such.
+
+    If user abuse reports does not clearly classify a comment as spam a class
+    is determined using Baysian inference. In this case no training occurs to
+    prevent circular training.
+
+    Returns a newly created or updated ClassifiedComment object.
+    As a side effect also set is_removed field of comment based on class.
+    """
+    from moderator.models import ClassifiedComment
+
+    if cls not in ['spam', 'ham', 'unsure', None]:
+        raise Exception("Unrecognized classifications.")
+
+    classified_comment, created = ClassifiedComment.objects.get_or_create(
+        comment=comment
+    )
+
+    if cls == 'spam' and classified_comment.cls != 'spam':
+        train(comment, is_spam=True)
+        comment.is_removed = True
+        comment.save()
+        classified_comment.cls = cls
+        classified_comment.save()
+        return classified_comment
+
+    if cls == 'ham' and classified_comment.cls != 'ham':
+        train(comment, is_spam=False)
+        comment.is_removed = False
+        comment.save()
+        classified_comment.cls = cls
+        classified_comment.save()
+        return classified_comment
+
+    if cls is None:
+        if comment.total_downvotes >= getattr(settings, 'MODERATOR',
+                                              DEFAULT_CONFIG)['ABUSE_CUTOFF']:
+            cls = 'spam'
+            train(comment, is_spam=True)
+            comment.is_removed = True
+            comment.save()
+            classified_comment.cls = cls
+            classified_comment.save()
+            return classified_comment
+        else:
+            cls = get_class(comment)
+            comment.is_removed = cls == 'spam'
+            comment.save()
+            classified_comment.cls = cls
+            classified_comment.save()
+            return classified_comment
+
+    return classified_comment
