c267d9d8a2dc2b4024f6e467111a939bcc2a7d61
Shaun Sephton <shaun@28lines.com>
2012-07-24 16:54:14 +0200

    reworked to use cleaner entry and continual evaluation of unsure comments

--- moderator/management/commands/classifycomments.py
+++ moderator/management/commands/classifycomments.py
@@ -10,12 +10,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Classifying, please wait...\n')
-        classified_comments = ClassifiedComment.objects.all()
-        unclassified_comments = Comment.objects.exclude(
+        classified_comments = ClassifiedComment.objects.filter(
+            cls__in=['spam', 'ham']
+        )
+        unsure_comments = Comment.objects.exclude(
             pk__in=[classified_comment.pk
                     for classified_comment in classified_comments]
         )
-        for comment in unclassified_comments:
-            cls = utils.get_class(comment)
-            ClassifiedComment.objects.create(comment=comment, cls=cls)
-            self.stdout.write('Classified %s as %s\n' % (comment, cls))
+
+        for comment in unsure_comments:
+            classified_comment = utils.classify_comment(comment)
+            self.stdout.write('Classified %s as %s\n'
+                              % (comment, classified_comment.cls))
