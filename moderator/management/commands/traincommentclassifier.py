05db310a4712cd3cbd12e558a8a05bb6b79a122d
Shaun Sephton <shaun@28lines.com>
2012-12-03 13:29:33 +0200

    finalized implementation

--- moderator/management/commands/traincommentclassifier.py
+++ moderator/management/commands/traincommentclassifier.py
@@ -1,17 +1,61 @@
 from django.core.management.base import BaseCommand
 from django.contrib.comments.models import Comment
-from moderator import utils
+from moderator import models, utils
+
+SAMPLE_COUNT = 10000
 
 
 class Command(BaseCommand):
-    help = 'Reinitializes(wipes) Baysian inference classifier and trains '\
-            'existing removed comments as being spam.'
+    help = 'Reinitializes Baysian inference classifier and trains sample of'\
+           'existing comments as ham(if not removed) or spam(if removed).'\
+           'WARNING: All previous classification data will be reset(wiped).'
 
     def handle(self, *args, **options):
+        # Clear previous classifications.
         utils.clear()
-        removed_comments = Comment.objects.filter(is_removed=True)
-        self.stdout.write('Learing from %s comments, please wait...\n' % len(removed_comments))
-        for comment in removed_comments:
-            utils.train(comment, is_spam=True)
-            self.stdout.write('"%s" learned as spam...\n' % comment.comment)
+        models.ClassifiedComment.objects.all().delete()
+
+        # Collect sample of ham and spam comments.
+        ham_comments = Comment.objects.filter(
+            is_removed=False
+        ).order_by('?')[:SAMPLE_COUNT]
+        spam_comments = Comment.objects.filter(
+            is_removed=True
+        ).order_by('?')[:SAMPLE_COUNT]
+        comments = list(ham_comments) + list(spam_comments)
+        comment_count = len(comments)
+
+        self.stdout.write('Learning from %s comments, please wait...' % comment_count)
+        self.stdout.flush()
+
+        perc = 0
+        ham = []
+        spam = []
+
+        # Train and group ham and spam comments as such.
+        for i, comment in enumerate(comments):
+            new_perc = int(i / (comment_count / 100.0))
+            if new_perc > perc:
+                perc = new_perc
+                self.stdout.write('.')
+                self.stdout.flush()
+            if comment.is_removed:
+                utils.train(comment, is_spam=True)
+                spam.append(models.ClassifiedComment(
+                    comment=comment,
+                    cls='spam',
+                ))
+            else:
+                utils.train(comment, is_spam=False)
+                ham.append(models.ClassifiedComment(
+                    comment=comment,
+                    cls='ham',
+                ))
+
+        # Bulk create classified comment objects for trained objects.
+        self.stdout.write('\nCreating classification objects, please wait...\n')
+        self.stdout.flush()
+        models.ClassifiedComment.objects.bulk_create(ham)
+        models.ClassifiedComment.objects.bulk_create(spam)
+
         self.stdout.write('Done!\n')
