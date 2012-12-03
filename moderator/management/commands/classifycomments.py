3f08ed54db07ad54492aa7d6def590034b8cf58e
Shaun Sephton <shaun@28lines.com>
2012-12-03 13:28:41 +0200

    allow to limit on count, cleanup, better filter

--- moderator/management/commands/classifycomments.py
+++ moderator/management/commands/classifycomments.py
@@ -1,24 +1,36 @@
-from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
 from django.contrib.comments.models import Comment
+from django.db.models import Q
 from moderator import utils
-from moderator.models import ClassifiedComment
 
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('-c', '--count',
+            dest='count',
+            type="int",
+            default=0,
+            help='Number of comments to classify.'),
+        )
     help = 'Classifies comments as either spam or '\
-           'ham using Bayesian inference.'
+           'ham using Bayesian inference and user reports.'
 
     def handle(self, *args, **options):
-        self.stdout.write('Classifying, please wait...\n')
-        classified_comments = ClassifiedComment.objects.filter(
-            cls__in=['spam', 'ham']
-        )
-        unsure_comments = Comment.objects.exclude(
-            pk__in=[classified_comment.pk
-                    for classified_comment in classified_comments]
-        )
+        # Collect all comments that hasn't already been classified or are classified as unsure.
+        # Order randomly so we don't rehash previously unsure classifieds when count limiting.
+        comments = Comment.objects.filter(Q(classifiedcomment__isnull=True) | Q(classifiedcomment__cls='unsure')).order_by('?')
+        if options['count']:
+            comments = comments[:options['count']]
+        comment_count = comments.count()
 
-        for comment in unsure_comments:
+        self.stdout.write('Classifying %s comments, please wait...' % comment_count)
+        self.stdout.flush()
+
+        for comment in comments:
             classified_comment = utils.classify_comment(comment)
-            self.stdout.write('Classified %s as %s\n'
-                              % (comment, classified_comment.cls))
+            self.stdout.write('%s,' % classified_comment.cls[0])
+            self.stdout.flush()
+
+        self.stdout.write('\nDone!\n')
